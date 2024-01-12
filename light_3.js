b49cd33e9682f20d941fa53d4cd1fc24bffda777
Ngugis904 <156364360+Ngugis904@users.noreply.github.com>
2024-01-12 10:53:19 +0300

    Update light_3.js

--- light_3.js
+++ light_3.js
@@ -55,7 +55,7 @@ const fetchLighthouseReports = async (urls, apiKey, Queue) => {
 
     progressBar.start(totalRequests, 0);
 
-    const queue = new Queue({ concurrency: 20 });
+    const queue = new Queue({ concurrency: 25 });
 
     const reportPromises = validUrls.map(url => queue.add(() => fetchLighthouseReport(url, apiKey, progressBar, Queue)));
 
