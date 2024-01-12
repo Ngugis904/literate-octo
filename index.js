674d70f5dce270a45cd88d4e2893592391d6308f
Ngugis904 <156364360+Ngugis904@users.noreply.github.com>
2024-01-12 20:51:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -103,5 +103,8 @@ const main_3 = async (realUrls) => {
         res.status(500).json({ error: 'Internal server error' });
     }
 });
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
 
 
